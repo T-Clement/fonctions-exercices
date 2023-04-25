b1f7029608cd78e627968df5efcdee1178108a03
Clément Toquet <toquet.clement@gmail.com>
2023-04-25 16:10:36 +0200

    creation function text toCamelCase

--- script.js
+++ script.js
@@ -3,13 +3,46 @@ console.log("1/ Implémentez une fonction qui transforme un texte en camelCase."
 // function textTocamelCase(text) {
     // itérer pour chaque mot à l'index 0 une majuscule
 // }
-console.log();
+function textTocamelCase(text) {
+    let words = text.split(/[-,_\s]+/g);
+    
+    words = words.map(function(word, index) {
+        if (index === 0) return word.toLowerCase();
+        return word.charAt(0).toUpperCase() +  word.slice(1).toLowerCase();
+    })
+    return words.join(" ");
+    // const newWords = [];
+    // words.forEach(word => {
+    //     newWords.push(word.toLowerCase());
+        
+    // });
+    // return newWords;
+
+
+
+
+    // for (let i = 0; i < words.length; i++) {
+    //     for (let j = 0; j < words[i]; j++) {
+    //         words[i][j].toUpperCase();
+    //     }
+    // }
+    // return words;
+
+    // for (let word of words) {
+    //     word = word.toUpperCase();
+    //     console.log(word);
+    //     // for (const index in word) {
+    //     // }
+    // }   
+
+}
+console.log(textTocamelCase("Cette phrase est un test"));
 
 // -----------------------------------
 console.log("2/ Implémentez une fonction qui retroune le plus grand mot d'un texte.");
 function getLongestWord(string) {
-    stringConverted = string.replace(/[^\w\s]+/g, '');
-    let array = stringConverted.split(" ");
+    // let stringConverted = string.replace(/[^\w\s]+/g, '');
+    let array = (string.replace(/[^\w\s]+/g, '')).split(" ");
     let maxLength = 0;
     let longestWord = ""; 
     for (const word of array) {
@@ -21,13 +54,69 @@ function getLongestWord(string) {
     }
     return longestWord;
 }
-console.log(getLongestWord("Ceci est une phrase de test qui vérifie si la fonction fonctionne."));
+console.log("Le mot le plus long est : ", getLongestWord("Ceci est une phrase de test qui vérifie si la fonction fonctionne."));
 
 // -----------------------------------
 console.log("3/ Implémentez une fonction qui converti un nombre de seconde en un tableau exprimant cette durée en secondes, minutes, heures, jours, semaines et années.");
+function putSecondesToTime(duration) {
+    
+//     let units = {
+//         "secondes": 0,
+//         "minutes": 0,
+//         "hours": 0,
+//         "days": 0,
+//         "weeks": 0,
+//         "years": 0
+//     };
+    
+//     const secondsInYear = 31557600;
+//     const secondsInWeek = 604800;
+//     const secondsInDay = 86400;
+//     const secondsInHour = 3600;
+//     const secondsInMinute = 60;
+
+//     while (duration >= 0) {
+//         while (secondsInYear < duration) {
+//             duration -= secondsInYear;
+//             units["years"]++; 
+//         }
+//         while (secondsInWeek < duration) {
+//             duration -= secondsInWeek;
+//             units["weeks"]++;
+//         }
+//         while (secondsInDay < duration) {
+//             duration -= secondsInDay;
+//             units["days"]++;
+//         }
+//         while (secondsInHour < duration) {
+//             duration -= secondsInHour;
+//             units["hours"]++;
+//         }
+//         while (secondsInMinute < duration) {
+//             duration -= secondsInMinute;
+//             units["minutes"]++;
+//         }
+//         units["seconds"] = duration;
+
+//     } return units;
+// }
+
+//     // let secondesInYears;
+//     // let secondesInMinutes = secondes / 60;
+//     // let secondesInHours = Math.floor(secondes / 3600);
+//     // let secondesInDays = Math.floor(secondes / 86400);
+//     // let secondesInYears = Math.floor(secondes / 31557600);
+//     // let restingSecondes = secondesInMi;  
+    
+//     // console.table();
+
+}
 
 
-console.log();
+// units["secondes"]++;
+// console.table(units);
+
+console.table(putSecondesToTime(5222013554));
 
 // -----------------------------------
 console.log("4/ Implémentez une fonction qui prend en paramètre un tableau retourné par la fonction précédente et retourne la chaîne de caractère correspondante.");
@@ -36,7 +125,9 @@ console.log();
 
 // -----------------------------------
 console.log("5/ Implémentez une fonction qui compte dans le texte en premier paramètre, le nombre d'occurence de la lettre en second paramètre.");
+function countLetterInText() {
 
+};
 console.log();
 
 // -----------------------------------
